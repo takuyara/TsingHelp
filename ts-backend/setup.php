9daf2bd50329282d0050caddf305edc31b7c548a
blutrex <blutrex@gmail.com>
2020-12-25 05:04:11 +0800

    setup

--- ts-backend/setup.php
+++ ts-backend/setup.php
@@ -2,13 +2,17 @@
 require 'ts-config.php';
 
 $SETUP_MARK = 'setup.txt';
+$TABLE_USERS = 'ts_users';
 error_reporting(E_ALL | E_STRICT);
 if (!file_exists($SETUP_MARK)) {
-	file_put_contents($SETUP_MARK, 'ts setup done');
+	if (!TS_DEV) {
+		file_put_contents($SETUP_MARK, 'ts setup done');
+	}
 	require 'ts-db.php';
-	if (TS_DEV)
-		$ts_db->exec('DROP TABLE IF EXISTS ts_users');
-	if (!$ts_db->exec('CREATE TABLE ts_users (uid VARCHAR(63), pwd VARCHAR(511), token VARCHAR(127), PRIMARY KEY (uid)) ENGINE=InnoDB DEFAULT CHARSET=' . TsConfig::get('db', 'charset'))) {
+	if (TS_DEV) {
+		$ts_db->exec('DROP TABLE IF EXISTS ' . $TABLE_USERS);
+	}
+	if (!$ts_db->exec('CREATE TABLE ' . $TABLE_USERS . ' (uid VARCHAR(63), pwd VARCHAR(511), token VARCHAR(127), PRIMARY KEY (uid)) ENGINE=InnoDB DEFAULT CHARSET=' . TsConfig::get('db', 'charset'))) {
 		echo 'could not create table ts_users';
 		exit(-1);
 	}
