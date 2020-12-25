f66d6d6a9a9041881570304354adda263ea315c0
takuyara <takuyara2136@gmail.com>
2020-12-25 14:11:58 +0800

    增加店铺信息

---
+++ ts-client/app/src/main/java/com/example/tsinghelp/ItemAdapter.java
@@ -0,0 +1,54 @@
+package com.example.tsinghelp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ItemAdapter extends BaseAdapter {
+    private List<String> listText;
+    private Context context;
+    public Map<Integer, Boolean> map = new HashMap<>();
+    public ItemAdapter(List<String> listText, Context context) {
+        this.listText = listText;
+        this.context = context;
+    }
+    @Override
+    public int getCount() {
+        return listText.size();
+    }
+    @Override
+    public Object getItem(int position) {
+        return listText.get(position);
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view;
+        if (convertView == null) {
+            view = View.inflate(context, R.layout.list_view_item, null);
+        } else {
+            view = convertView;
+        }
+        TextView radioText = (TextView)view.findViewById(R.id.tv_check_text);
+        radioText.setText(listText.get(position));
+        final CheckBox checkBox =(CheckBox)view.findViewById(R.id.rb_check_button);
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) map.put(position, true); else map.remove(position);
+            }
+        });
+        checkBox.setSelected(map != null && map.containsKey(position));
+        return view;
+    }
+}
